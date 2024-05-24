3f6b1215d71e9c2219e3109565cabe78ec3d506d
eyadhashim <hashimeyad648@gmail.com>
2024-05-24 08:16:31 -0700

    0

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Declaration of function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Returns an object represented by a JSON
+
+    Args:
+        my_obj: the object
+        filename: the name of file
+    """
+    with open(filename, "w") as my_file:
+        json.dump(my_obj, my_file)
